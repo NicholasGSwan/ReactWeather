9e47d1065be2f35a613f90374101604d77249605
Nicholas Swan <nicholasgswan@gmail.com>
2018-05-10 00:10:09 -0500

    -prevent default for search form -setup ajax request for openweather api

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY ='3022409fb7de769280b99ebd1b796837'
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city)  {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
